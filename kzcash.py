cb85c4cd346749c69ebf3b68612db280ad3fb59b
Almrina <162822710+Almrina@users.noreply.github.com>
2024-03-12 22:14:53 +0600

    kzcash.py

--- kzcash.py
+++ kzcash.py
@@ -26,58 +26,58 @@ def addressBalance(args):
 @bot.message_handler(commands=['getnewaddress'])
 def get_new_address(message):
     new_address = rpc_connection.getnewaddress()
-    bot.reply_to(message, f"Новый адрес: {new_address}")
+    bot.reply_to(message, f"New address: {new_address}")
 
 @bot.message_handler(commands=['getbalance'])
 def get_balance(message):
     balance = rpc_connection.getbalance()
-    bot.reply_to(message, f"Общий баланс кошелька: {balance}")
+    bot.reply_to(message, f"Total wallet balance: {balance}")
 
 
 @bot.message_handler(commands=['send'])
 def send_coins(message):
     args = message.text.split()[1:]
     if len(args) != 3:
-        bot.reply_to(message, "Шаблон: /send <адрес отправителя> <адрес получателя> <сумма>")
+        bot.reply_to(message, "Template: /send <sender address> <recipient address> <amount>")
         return
     sender_address, receiver_address, amount = args
 
     try:
         inputs = rpc_connection.listunspent(0, 9999, [sender_address])
     except JSONRPCException:
-        bot.reply_to(message, f"Неправильный адрес кошелька отправителя")
+        bot.reply_to(message, f"Invalid sender wallet address")
         return
 
     for i in (0, len(inputs) - 1):
         temp = inputs[i]
         if float(float(temp.get("amount"))) > (float(amount)+0.001):
             break
-        bot.reply_to(message, "Недостаточно средств")
+        bot.reply_to(message, "Insufficient funds")
         return
     fee = float(temp.get("amount")) - float(amount) - 0.001
     inputForTransaction = {"txid":temp.get("txid"), "vout": temp.get("vout")}
     try:
         createTransaction = rpc_connection.createrawtransaction([inputForTransaction], {receiver_address:amount, sender_address:fee})
     except JSONRPCException:
-        bot.reply_to(message, f"Неправильный адрес кошелька получателя")
+        bot.reply_to(message, f"Invalid recipient wallet address")
         return
     signTransaction = rpc_connection.signrawtransaction(createTransaction)
     receivedHex = signTransaction.get("hex")
     txid = rpc_connection.sendrawtransaction(receivedHex)
-    bot.reply_to(message, f"Монеты отправлены получателю! ID транзакции: {txid}")
+    bot.reply_to(message, f"Coins are sent to the recipient! Transaction ID: {txid}")
 
 @bot.message_handler(commands=['getaddressbalance'])
 def get_address_balance(message):
     args = message.text.split()[1:]
     if len(args) != 1:
-        bot.reply_to(message, "Шаблон: /getaddressbalance <адрес кошелька>")
+        bot.reply_to(message, "Template: /getaddressbalance <wallet address>")
         return
     try:
         balance = addressBalance(args)
     except JSONRPCException:
-        bot.reply_to(message, f"Неправильный адрес кошелька ")
+        bot.reply_to(message, f"Invalid wallet address ")
         return
-    bot.reply_to(message, f"Баланс адреса: {balance} KZC")
+    bot.reply_to(message, f"Address balance: {balance} KZC")
 
 
 @bot.message_handler(content_types=['text'])
